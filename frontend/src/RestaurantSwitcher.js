e3d1183d816000de7ab3f88ad5ed9ccd36fe1261
Beast\traceur.gan <traceur.gan@gmail.com>
2020-05-04 15:24:04 +0800

    Enabled population of list from database

--- frontend/src/RestaurantSwitcher.js
+++ frontend/src/RestaurantSwitcher.js
@@ -2,29 +2,43 @@ import React, { Component } from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 
 class RestaurantSwitcher extends Component {
-  state = { theme: null,
-            list: null}
+
+  state = { selected: '',
+            list: ''}
 
   constructor(props){
     super(props);
-    var list = this.props.ListOfRestaurants
+    this.getRestaurantList = this.getRestaurantList.bind(this);
+    this.state.selected = '';
+    this.state.list = '';
   }
   
-  chooseTheme = (theme, evt) => {
+  
+  selectRestaurant = (restaurant, evt) => {
     evt.preventDefault();
-    if (theme.toLowerCase() === 'reset') { theme = null }
-    this.setState({ theme });
+    if (restaurant.toLowerCase() === 'reset') { restaurant = null }
+    this.setState({ selected: restaurant });
   }
 
-  chooseRestaurant = () => {
+  getRestaurantList() {
+    fetch('http://localhost:3000/q1')
+      .then(res => res.json())
+      .then ((out) => {
+        this.setState({list: out});
+        //console.log(this.state.list);
+      })
+  
+  }
 
+  componentDidMount() {
+    this.getRestaurantList();
   }
   
   render() {
   
-    const { theme,selection } = this.state;
-    const themeClass = theme ? theme.toLowerCase() : 'default';
-    
+    const selected = this.state.selected;
+    const restaurantClass = selected ? selected.toLowerCase() : 'default';
+    var listofRestaurant = [];
     const parentContainerStyles = {
       position: 'absolute',
       height: '100%',
@@ -40,18 +54,20 @@ class RestaurantSwitcher extends Component {
       verticalAlign: 'middle'
     };
     
+    if (this.state.list != '') {
+      this.state.list.forEach(restaurant => {
+      listofRestaurant.push(<Dropdown.Item eventKey={restaurant.eid} id= "rest1" onSelect={this.selectRestaurant}>{restaurant.eid}</Dropdown.Item>);
+      });
+    }
+
     return (
       <div>
         <div>
-          <span className={`h1 center-block text-center text-${theme ? themeClass : 'muted'}`} style={{ marginBottom: 25 }}>{theme || 'Default'}</span>
+          <span className={`h1 center-block text-center text-${selected ? restaurantClass : 'muted'}`} style={{ marginBottom: 25 }}>{selected || 'Default'}</span>
           
-          <div className="center-block text-center">
-            <DropdownButton title={`${theme || 'Default'} Theme`}>
-              <Dropdown.Item eventKey="Primary" onSelect={this.chooseTheme}>Primary Theme</Dropdown.Item>
-              <Dropdown.Item eventKey="Danger" onSelect={this.chooseTheme}>Danger Theme</Dropdown.Item>
-              <Dropdown.Item eventKey="Success" onSelect={this.chooseTheme}>Success Theme</Dropdown.Item>
-              <Dropdown.Item divider />
-              <Dropdown.Item eventKey="Reset" onSelect={this.chooseTheme}>Default Theme</Dropdown.Item>
+          <div id="switch" className="center-block text-center">
+            <DropdownButton title={`${selected || 'Select Restaurant'}`}>
+              {listofRestaurant}
             </DropdownButton>
           </div>
           
